fee3de740b7833d4145d7825f9f7128828a194b4
NIKESH GHIMIRE <nikeshghimire77@gmail.com>
2020-07-31 11:31:34 -0500

    Fixed bugs in contactContext + added funtionalities for filter

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,34 +1,16 @@
-import React, { useContext, useRef, useEffect } from 'react';
+import React, { useContext } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactFilter = () => {
-  const contactContext = useContext(ContactContext);
-  const text = useRef('');
-
-  const { filterContacts, clearFilter, filtered } = contactContext;
-
-  useEffect(() => {
-    if (filtered === null) {
-      text.current.value = '';
-    }
-  });
+  const { filterContacts, clearFilter } = useContext(ContactContext);
 
   const onChange = (e) => {
-    if (text.current.value !== '') {
-      filterContacts(e.target.value);
-    } else {
-      clearFilter();
-    }
+    e.target.value !== '' ? filterContacts(e.target.value) : clearFilter();
   };
 
   return (
-    <form>
-      <input
-        ref={text}
-        type='text'
-        placeholder='Filter Contacts...'
-        onChange={onChange}
-      />
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input type='text' placeholder='Filter Contacts...' onChange={onChange} />
     </form>
   );
 };
--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -40,6 +40,7 @@ export const ContactForm = () => {
     }
     clearAll();
   };
+  
   const clearAll = () => {
     clearCurrent();
   };
